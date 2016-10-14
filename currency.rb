05466a83d45a70a73b0bf5ecbf6f54be557b05b7
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-14 15:54:18 -0400

    I removed the equal variable, renamed code and value variables, and removed the equal/not equal methods. I refactored the == method and added the + and - methods, along with applicable error message.

--- currency.rb
+++ currency.rb
@@ -1,34 +1,47 @@
+require_relative "./error"  # => true
+
 class Currency
 
-  def initialize
-    @amount = 100
-    @currency_code = "USD"
-    @equal = true           #
-  end
+  def initialize(value, code)
+    @value = value             # => 100, 100
+    @code = code               # => "USD", "EUR"
+  end                          # => :initialize
 
-  def amount
-    @amount
-  end
+  def value
+    @value   # => 100, 100
+  end        # => :value
 
-  def currency_code
-    @currency_code
-  end
+  def code
+    @code   # => "EUR", "USD", "EUR", "USD"
+  end       # => :code
 
-  def equal
-    @equal
-  end
+  def ==(object)
+    object.value == value && object.code == code ? true : false  # => false
+  end                                                            # => :==
 
-  def not_equal
-    !@equal
-  end
+  def +(object)
+    raise Error if object.code != code  # ~> Error: DifferentCurrencyCodeError
+    object.value + value
+  end                                   # => :+
 
-  def equal?(user_value, user_code)
-    if user_value == amount && user_code == currency_code
-      equal
-    else
-      not_equal
-    end
-  end
+  def -(object)
+    raise Error if object.code != code
+    object.value - value >= 0 ? object.value - value : 0
+  end                                                     # => :-
 
 
-end
+end  # => :-
+
+
+
+usd_100 = Currency.new(100, "USD")  # => #<Currency:0x007fbbd9963970 @value=100, @code="USD">
+usd_200 = Currency.new(100, "EUR")  # => #<Currency:0x007fbbd9962ac0 @value=100, @code="EUR">
+usd_100.==(usd_200)                 # => false
+usd_100.+(usd_200)
+usd_100.-(usd_100)
+
+# ~> Error
+# ~> DifferentCurrencyCodeError
+# ~>
+# ~> /Users/bmangelsen/code/github/101416-currency-converter/currency.rb:23:in `+'
+# ~> /Users/bmangelsen/code/github/101416-currency-converter/currency.rb:40:in `<main>'
