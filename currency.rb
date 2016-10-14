4127a56ea6257a49908b0024aca4bd3e2941d89a
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-14 14:17:10 -0400

    I added the variable equal, created methods returning the values of amount, currency code, and equal/not equal, and created a method to test if another currency object is equal to my currency object.

--- currency.rb
+++ currency.rb
@@ -3,6 +3,32 @@ class Currency
   def initialize
     @amount = 100
     @currency_code = "USD"
+    @equal = true           #
   end
+
+  def amount
+    @amount
+  end
+
+  def currency_code
+    @currency_code
+  end
+
+  def equal
+    @equal
+  end
+
+  def not_equal
+    !@equal
+  end
+
+  def equal?(user_value, user_code)
+    if user_value == amount && user_code == currency_code
+      equal
+    else
+      not_equal
+    end
+  end
+
 
 end
