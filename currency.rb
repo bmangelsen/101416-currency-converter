2425565a9411c274b5367774c35db0da593f8618
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-15 11:17:41 -0400

    I added the multiplication method which returns a new object

--- currency.rb
+++ currency.rb
@@ -1,47 +1,46 @@
-require_relative "./error"  # => true
+require_relative "./error"
 
 class Currency
 
   def initialize(value, code)
-    @value = value             # => 100, 100
-    @code = code               # => "USD", "EUR"
-  end                          # => :initialize
+    @value = value
+    @code = code
+  end
 
   def value
-    @value   # => 100, 100
-  end        # => :value
+    @value
+  end
 
   def code
-    @code   # => "EUR", "USD", "EUR", "USD"
-  end       # => :code
+    @code
+  end
 
   def ==(object)
-    object.value == value && object.code == code ? true : false  # => false
-  end                                                            # => :==
+    object.value == value && object.code == code ? true : false
+  end
 
   def +(object)
-    raise Error if object.code != code  # ~> Error: DifferentCurrencyCodeError
+    raise Error if object.code != code
     object.value + value
-  end                                   # => :+
+  end
 
   def -(object)
     raise Error if object.code != code
     object.value - value >= 0 ? object.value - value : 0
-  end                                                     # => :-
+  end
+
+  def *(factor)
+    Currency.new(value * factor, code)
+  end
 
 
-end  # => :-
+end
 
 
 
-usd_100 = Currency.new(100, "USD")  # => #<Currency:0x007fbbd9963970 @value=100, @code="USD">
-usd_200 = Currency.new(100, "EUR")  # => #<Currency:0x007fbbd9962ac0 @value=100, @code="EUR">
-usd_100.==(usd_200)                 # => false
+usd_100 = Currency.new(100, "USD")
+usd_200 = Currency.new(200, "USD")
+usd_100.==(usd_200)
 usd_100.+(usd_200)
 usd_100.-(usd_100)
-
-# ~> Error
-# ~> DifferentCurrencyCodeError
-# ~>
-# ~> /Users/bmangelsen/code/github/101416-currency-converter/currency.rb:23:in `+'
-# ~> /Users/bmangelsen/code/github/101416-currency-converter/currency.rb:40:in `<main>'
+usd_423 = usd_100.*4.23
