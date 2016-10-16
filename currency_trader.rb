4100f8e3a6a8f2f769efa10edfe765cb5857a0f0
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-16 12:09:32 -0400

    I removed my_value method as it was unnecessary, and I added the best_investment? method which will return the country code that has the best return value.

--- currency_trader.rb
+++ currency_trader.rb
@@ -17,13 +17,27 @@ class CurrencyTrader
     @converter2.conversion_rates
   end
 
-  def my_value
-    @currency.value
-  end
-
   def my_code
     @currency.code
   end
 
+  def best_investment?
+    converter1_comparative_rates = []
+    converter2_comparative_rates = []
+    rate_differences = []
+    currency_codes = converter1_rates.keys
 
+      converter1_rates.keys.each do |x|
+        converter1_comparative_rates << converter1_rates[x] / converter1_rates[my_code]
+      end
+      converter2_rates.keys.each do |x|
+        converter2_comparative_rates << converter2_rates[x] / converter2_rates[my_code]
+      end
+
+      converter1_comparative_rates.each_index do |x|
+        rate_differences << converter1_comparative_rates[x] / converter2_comparative_rates[x]
+      end
+
+      currency_codes[rate_differences.rindex(rate_differences.max)]
+  end
 end
